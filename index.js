123d9638c00fcb27edd04e718d63a58b37d2fd8a
TwilightCode <twilightcode@users.noreply.github.com>
2021-02-16 00:21:17 +0200

    Fixed typo in linter config that broke indent

--- index.js
+++ index.js
@@ -16,96 +16,96 @@ app.use(cors())
 app.use(express.json())
 
 morgan.token('person', function getId(req) {
-                      return JSON.stringify(req.body)
+  return JSON.stringify(req.body)
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
 
 app.get('/api/persons', (request, response) => {
-                      Person.find({}).then(persons => {
-                                            response.json(persons)
-                      })
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-                      const id = request.params.id
-                      Person.findById(id).then(person => {
-                                            if (person) {
-                                                                  response.json(person)
-                                            } else {
-                                                                  response.status(404).end()
-                                            }
-                      }).catch(error => { next(error) })
+  const id = request.params.id
+  Person.findById(id).then(person => {
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  }).catch(error => { next(error) })
 })
 
 
 app.get('/info/', (request, response, next) => {
-                      Person.find({}).then(results => {
-                                            const amount = results.length
-                                            response.send(`<p>Phonebook has info for ${amount} people </p>
+  Person.find({}).then(results => {
+    const amount = results.length
+    response.send(`<p>Phonebook has info for ${amount} people </p>
         <p>${new Date()}</p>`)
-                      }).catch(error => { next(error) })
+  }).catch(error => { next(error) })
 
 
 
 })
 
 app.post('/api/persons', (request, response, next) => {
-                      const body = request.body
-                      /*
-        if (!body.name || !body.number) {
-            return response.status(400).json({
-                error: 'Either name or number is missing'
-            })
-        }*/
-                      const person = new Person({
-                                            //id: generateId(),
-                                            name: body.name,
-                                            number: body.number
-                      })
+  const body = request.body
+  /*
+if (!body.name || !body.number) {
+return response.status(400).json({
+error: 'Either name or number is missing'
+})
+}*/
+  const person = new Person({
+    //id: generateId(),
+    name: body.name,
+    number: body.number
+  })
 
-                      person.save().then(savedPerson => {
-                                            persons = persons.concat(savedPerson)
-                                            console.log('Saved: ' + savedPerson)
-                                            response.json(savedPerson)
-                      })
-                                            .catch(error => {next(error)})
+  person.save().then(savedPerson => {
+    persons = persons.concat(savedPerson)
+    console.log('Saved: ' + savedPerson)
+    response.json(savedPerson)
+  })
+    .catch(error => { next(error) })
 
 })
 app.put('/api/persons/:id', (request, response, next) => {
-                      const body = request.body
+  const body = request.body
 
-                      const person = {
-                                            name: body.name,
-                                            number: body.number,
-                      }
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
 
-                      Person.findByIdAndUpdate(request.params.id, person, { new: true })
-                                            .then(updatedPerson => {
-                                                                  response.json(updatedPerson)
-                                            })
-                                            .catch(error => next(error))
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-                      Person.findByIdAndRemove(request.params.id)
-                                            .then(result => {
-                                                                  response.status(204).end()
-                                            }).catch(error => next(error))
+  Person.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    }).catch(error => next(error))
 
 
 })
 
 const errorHandler = (error, request, response, next) => {
-                      console.error(error.message)
-                      if (error.name === 'CastError') {
-                                            return response.status(400).send({ error: 'malformatted id' })
-                      }
-                      next(error)
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
 }
 //ensure this is loaded as the last middleware
 app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-                      console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- models/person.js
+++ models/person.js
@@ -5,19 +5,19 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-    .then(result => { console.log('connected to MongoDB') }).catch((error) => { console.log('error connecting to MongoDB:', error.message) })
+  .then(() => { console.log('connected to MongoDB') }).catch((error) => { console.log('error connecting to MongoDB:', error.message) })
 
 const personSchema = new mongoose.Schema({
-    name: {type: String, required:true, unique:true, minlenght: 3},
-    number: {type: String, required:true, unique:true, minlength: 8}
+  name: { type: String, required: true, unique: true, minlenght: 3 },
+  number: { type: String, required: true, unique: true, minlength: 8 }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 personSchema.plugin(mongooseUniqueValidator)
 module.exports = mongoose.model('Person', personSchema)
